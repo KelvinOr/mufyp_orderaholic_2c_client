410fc7c3ef394290465798c0e462ed9928e59e9b
Akatsuki <55689974+KelvinOr@users.noreply.github.com>
2022-10-21 09:52:41 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Orderaholic',
       theme: ThemeData(
         primaryColor: PrimaryColor,
         scaffoldBackgroundColor: PrimaryColor,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: RegisterPage(),
-        ),
-      ),
+      home: RegisterPage(),
     );
   }
 }
